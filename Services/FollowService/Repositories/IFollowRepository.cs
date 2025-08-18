feebbb7acae8af1cf298c788086162ad46e1ee8a
kiriko <2750469650@qq.com>
2025-08-18 15:46:39 +0800

    新增统计关注被关注人数和分页返回关注者被关注者列表的api和对应的测试

--- Services/FollowService/Repositories/IFollowRepository.cs
+++ Services/FollowService/Repositories/IFollowRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using FollowService.Models;
 
 namespace FollowService.Repositories
@@ -8,5 +9,9 @@ namespace FollowService.Repositories
         Task<Follow> CreateFollowAsync(Follow follow);
         Task<Follow> GetFollowAsync(int followerId, int followeeId);
         Task<bool> DeleteFollowAsync(int followerId, int followeeId);
+        Task<int> GetFollowingCountAsync(int userId);
+        Task<int> GetFollowerCountAsync(int userId);
+        Task<List<Follow>> GetFollowingListAsync(int userId, int page, int pageSize);
+        Task<List<Follow>> GetFollowerListAsync(int userId, int page, int pageSize);
     }
 }
