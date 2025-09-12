f13d657565ab4b41772d5db86c04d25b3fdb15b3
kiriko <2750469650@qq.com>
2025-09-12 17:11:11 +0800

    更新部分服务的数据库映射,修复了私信服务的id问题

--- Services/NoticeService/Models/Comment.cs
+++ Services/NoticeService/Models/Comment.cs
@@ -1,15 +1,48 @@
-﻿namespace NoticeService.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NoticeService.Models
 {
+    [Table("COMMENTS")]
     public class Comment
     {
+        [Key]
+        [Column("COMMENT_ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CommentId { get; set; }
-        public int PostId { get; set; }
-        public int UserId { get; set; }
-        public string Content { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public int Likes { get; set; }
-        public int Dislikes { get; set; }
-        public bool IsDeleted { get; set; }
-        public int TargetUserId { get; set; }
+
+        [Column("POST_ID")]
+        public int? PostId { get; set; }
+
+        [Column("USER_ID")]
+        public int? UserId { get; set; }
+
+        [Column("CONTENT")]
+        [MaxLength(4000)]  // 对应CLOB
+        public string? Content { get; set; }
+
+        [Column("CREATED_AT")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Column("LIKES")]
+        public int? Likes { get; set; }
+
+        [Column("DISLIKES")]
+        public int? Dislikes { get; set; }
+
+        // 数据库中的NUMBER字段
+        [Column("IS_DELETED")]
+        public int IsDeletedNumber { get; set; }
+
+        [NotMapped]
+        public bool IsDeleted
+        {
+            get => IsDeletedNumber != 0;
+            set => IsDeletedNumber = value ? 1 : 0;
+        }
+
+        [Column("TARGET_USER_ID")]
+        public int? TargetUserId { get; set; }
     }
 }
